7d5326872d0ce37a176707fed13486ed7c9ccb66
dengzhiteng <mrdengzhiteng@163.com>
2020-08-15 12:56:09 +0800

    删除

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -15,30 +15,49 @@ class TodoList extends Component {
         </div>
         <ul>
           {this.state.list.map((item, index) => {
-            return <li key={index}>{item}</li>;
+            return (
+              <li key={index}>
+                {item}
+                <button onClick={this.handleItemDel.bind(this, index)}>
+                  删除
+                </button>
+              </li>
+            );
           })}
         </ul>
       </div>
     );
   }
+  //   定义构造函数
   constructor(props) {
+    //  接受传值
     super(props);
     this.state = {
       list: [],
-      keyWords: "关键字",
+      keyWords: "",
     };
   }
+  // 输入关键字
   inputChangedHandler = (event) => {
     this.setState({
       keyWords: event.target.value,
     });
   };
+  // 提交
   handleSubmit(event) {
     const keyWords = this.state.keyWords;
     if (!keyWords) return;
     this.setState({
       keyWords: "",
       list: [...this.state.list, keyWords],
+    });
+  }
+  //   删除 li
+  handleItemDel(index, event) {
+    const listCopy = [...this.state.list];
+    listCopy.splice(index, 1);
+    this.setState({
+      list: listCopy,
     });
   }
 }
